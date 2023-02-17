227753483a6b855eadc12e12c63d05781829cc09
Draugelis <github@pysc.eu>
2023-02-17 23:36:44 +0200

    add video detail test

---
+++ fetchers/video_details_test.go
@@ -0,0 +1,64 @@
+package fetchers
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/h2non/gock"
+)
+
+func TestGetVideoDetails(t *testing.T) {
+	defer gock.Off()
+
+	mock := []byte(`
+	{
+		"kind": "youtube#videoListResponse",
+		"etag": "xEY3cMNpcoxXuz2o2E8UEQCRx_U",
+		"items": [
+			{
+				"kind": "youtube#video",
+				"etag": "Ck-MdM2DnK0T8t1MUQ7nE9VHVRY",
+				"id": "jNQXAC9IVRw",
+				"snippet": {
+					"publishedAt": "2005-04-24T03:31:52Z",
+					"channelId": "UC4QobU6STFB0P71PMvOGN5A",
+					"title": "Me at the zoo"
+				},
+				"contentDetails": {
+					"duration": "PT19S"
+				},
+				"statistics": {
+					"viewCount": "256993049",
+					"likeCount": "13206712",
+					"favoriteCount": "0",
+					"commentCount": "11271468"
+				}
+			}
+		],
+		"pageInfo": {
+			"totalResults": 1,
+			"resultsPerPage": 1
+		}
+	}`)
+	var wantedDetails VideoDetails
+	json.Unmarshal(mock, &wantedDetails)
+
+	var want Video
+	want.Id = "jNQXAC9IVRw"
+	want.Url = "https://youtube.com/watch?v=jNQXAC9IVRw"
+	want.Title = wantedDetails.Items[0].Snippet.Title
+	want.Duration = wantedDetails.Items[0].ContentDetails.Duration
+	want.ViewCount = wantedDetails.Items[0].Statistics.ViewCount
+	want.LikeCount = wantedDetails.Items[0].Statistics.LikeCount
+	want.CommentCount = wantedDetails.Items[0].Statistics.CommentCount
+
+	gock.New("https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id=jNQXAC9IVRw&key=someKey").
+		Reply(200).
+		JSON(mock)
+
+	res, err := GetVideoDetails("jNQXAC9IVRw", "someKey")
+
+	if res != want || err != nil {
+		t.Errorf("Failed GetVideoDetails('jNQXAC9IVRw', 'someKey'). Got: %v. Want: %v", res, want)
+	}
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/gin-gonic/gin v1.8.2
+	github.com/h2non/gock v1.2.0
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/exp v0.0.0-20230213192124-5e25df0256eb
 	golang.org/x/net v0.5.0
@@ -15,6 +16,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
 	github.com/goccy/go-json v0.9.11 // indirect
+	github.com/h2non/parth v0.0.0-20190131123155-b4df798d6542 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
